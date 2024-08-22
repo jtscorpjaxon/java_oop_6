1cffa1d50200daa274efdfe7f3f798b506bb8c4d
jtscorpjaxon <jtscorp@mail.ru>
2024-08-22 11:20:45 +0500

    main init

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Rectangle rect = new Rectangle(12,23);
+        System.out.println(rect.area());
+        System.out.println(rect.perimeter());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Triangle uchburchak = new Triangle(12,23,34);
+        System.out.println(uchburchak.area());
+        System.out.println(uchburchak.perimeter());
+
+        Trapezium trapetsiya=new Trapezium(5,7,7);
+        System.out.println(trapetsiya.area());
+        System.out.println(trapetsiya.perimeter());
+
+        Cat kisa = new Cat("Kisa");
+        System.out.println(kisa.getName());
+        kisa.sound();
+
+        Dog olopar=new Dog("Olopar");
+        System.out.println(olopar.getName());
+        olopar.sound();
+
+        Duck sariq=new Duck("Sariq");
+        System.out.println(sariq.getName());
+        sariq.sound();
     }
 }
